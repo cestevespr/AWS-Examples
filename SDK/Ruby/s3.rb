4ef8672decd1b3b7250015a5f40e4a7943044743
Christopher Esteves <cestevespr@yahoo.com>
2024-03-13 14:50:08 +0000

    Attempt 4

--- SDK/Ruby/s3.rb
+++ SDK/Ruby/s3.rb
@@ -1,37 +1,45 @@
-require 'aws-sdk-s3'
-require 'pry'
-require 'securerandom'
+# Required libraries
+require 'aws-sdk-s3'  # AWS SDK for S3 operations
+require 'pry'        # Interactive shell for debugging
+require 'securerandom' # For generating secure random strings (UUIDs)
 
-bucket_name = ENV['BUCKET_NAME']
-region = 'us-east-1'
+# S3 Bucket configuration
+bucket_name = ENV['BUCKET_NAME']  # Fetching bucket name from environment variables
+region = 'us-east-1'           # AWS region where the bucket will be created
 
-client = Aws::S3::Client.new
+# Initialize AWS S3 client
+client = Aws::S3::Client.new      # Creating a new instance of the S3 client
 
+# Create S3 bucket
 resp = client.create_bucket({
-  bucket: bucket_name, 
+  bucket: bucket_name,            # Name of the bucket
   create_bucket_configuration: {
-    location_constraint: region
+    location_constraint: region   # Region for the bucket
   }
 })
-binding.pry
+# binding.pry
 
-number_of_files = 1+ rand(6)
-puts "number_of.files: #{number_of_files}"
+# Determine the number of files to create and upload
+number_of_files = 1 + rand(6)     # Generating a random number between 1 and 6
+puts "number_of_files: #{number_of_files}"
 
-number_of_files.times.each so |i|
-    puts "i: #{i}.txt"
-    filename = "file_#{i}.txt"
-    output_path = "/tmp/#{filename}"
+# Loop to create and upload each file
+number_of_files.times.each do |i|
+  puts "i: #{i}"
+  filename = "file_#{i}.txt"      # Generating a filename for each file
+  output_path = "/tmp/#{filename}"# Specifying the output path for the file
 
-    File.open(output_path, "w") do |f|
-        f.write securerandom.uuid
-    end
+  # Writing a unique UUID to each file
+  File.open(output_path, "w") do |f|
+    f.write(SecureRandom.uuid)    # Write a random UUID to the file
+  end
 
-    File.open(output_path, 'rb') do |file|
-    s3.put_object(
-        bucket: bucket_name,
-        key: filename,
-        body: f
+  # Open and read the file in binary mode, then upload it to S3
+  File.open(output_path, 'rb') do |f|  # 'rb' mode for reading binary data
+    client.put_object(                # Uploading the file to S3
+      bucket: bucket_name,            # Bucket to upload to
+      key: filename,                  # Key (filename) for the object in the bucket
+      body: f                         # File content
     )
-    end
+  end
 end
