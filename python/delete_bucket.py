98826ce61f7e5f8b9b8aa3a8a692c00e3c9815b4
Christopher Esteves <cestevespr@yahoo.com>
2024-03-16 16:26:11 +0000

    minor changes to python code

--- python/delete_bucket.py
+++ python/delete_bucket.py
@@ -3,5 +3,5 @@ region = "us-east-1"  # Example: 'us-west-2' for Oregon
 bucket_name = "my-new-bucket-name"  # Ensure this is unique across all existing bucket names
 
 # Create the S3 bucket
-s3.create_bucket(Bucket=bucket_name,
+s3.delete_bucket(Bucket=bucket_name,
                  CreateBucketConfiguration={'LocationConstraint': region})
--- python/s3.py
+++ python/s3.py
@@ -1,6 +1,6 @@
 ##test 
-
 import boto3
 
 # Let's use Amazon S3
-s3 = boto3.resource('s3')
+s3 = boto3.client('s3')
+
