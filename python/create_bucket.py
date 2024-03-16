493a59fa287bb0952cca0682e37975c696efca21
Christopher Esteves <cestevespr@yahoo.com>
2024-03-16 16:43:37 +0000

    changed region to follow defaults of aws creds

--- python/create_bucket.py
+++ python/create_bucket.py
@@ -1,7 +1,6 @@
 # Specify the region for the bucket
-region = "us-east-1"  # Example: 'us-west-2' for Oregon
+
 bucket_name = "my-new-bucket-name"  # Ensure this is unique across all existing bucket names
 
 # Create the S3 bucket
-s3.create_bucket(Bucket=bucket_name,
-                 CreateBucketConfiguration={'LocationConstraint': region})
+s3.create_bucket(Bucket=bucket_name)
